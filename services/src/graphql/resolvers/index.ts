752c60d72c82d6c5d5b831f173da301209092a77
Nitin <nitinpatel@rippling.com>
2021-08-14 09:52:52 +0530

    graphql stuff

---
+++ services/src/graphql/resolvers/index.ts
@@ -0,0 +1,9 @@
+import * as Query from "./Query";
+import Chef from "./Chef";
+
+const resolvers = {
+  Chef,
+  Query,
+};
+
+export default resolvers;
--- services/src/server/startServer.ts
+++ services/src/server/startServer.ts
@@ -1,8 +1,14 @@
 import accessEnv from "#root/helpers/accessEnv";
 import * as express from "express";
 import * as cors from "cors";
+import { ApolloServer } from "apollo-server-express";
+
+import resolvers from "#root/graphql/resolvers";
+import typeDefs from "#root/graphql/typeDefs";
 
 const PORT = Number(accessEnv("PORT", "7000"));
+
+const apolloServer = new ApolloServer({ resolvers, typeDefs });
 
 const app = express();
 
@@ -11,6 +17,8 @@ app.use(
     origin: "*",
   })
 );
+
+apolloServer.applyMiddleware({ app, path: "/graphql" });
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
